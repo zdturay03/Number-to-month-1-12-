d85c8eeb77d481a6e744f4e4002c63a8fca51a96
zdturay03 <turayzandradanes@gmail.com>
2024-07-02 09:07:06 +0000

    done adding output

--- DayInMonth.java
+++ DayInMonth.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DaysInMonth {
+public class DayInMonth {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a month number (1-12): ");
